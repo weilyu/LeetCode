8e7712a249bc42f0645004c65b674e91413c46af
Wei Lyu <lvwei1001@live.com>
2016-02-17 14:59:40 +0800

    sovled Bulb Switcher problem

--- src/solutions/BulbSwitcher.java
+++ src/solutions/BulbSwitcher.java
@@ -7,28 +7,27 @@ package solutions;
  * There are n bulbs that are initially off. You first turn on all the bulbs. Then, you turn off every second bulb.
  * On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the nth round,
  * you only toggle the last bulb. Find how many bulbs are on after n rounds.
- *
  */
 public class BulbSwitcher {
     public int bulbSwitch(int n) {
-        int[] bulbs = new int[n+1];
+        int[] bulbs = new int[n + 1];
 
         //light all bulbs on the first round
-        for (int i =1; i<n+1;i++) {
+        for (int i = 1; i < n + 1; i++) {
             bulbs[i] = 1;
         }
 
         //second to nth round
-        for (int i = 2; i<=n; i++) {
-            for (int k = i; k<=n; k+=i) {
+        for (int i = 2; i <= n; i++) {
+            for (int k = i; k <= n; k += i) {
                 bulbs[k]++;
             }
         }
 
         //check light bulbs
         int count = 0;
-        for (int i = 1; i<n+1;i++) {
-            if (bulbs[i]%2 != 0) {
+        for (int i = 1; i < n + 1; i++) {
+            if (bulbs[i] % 2 != 0) {
                 count++;
             }
         }
