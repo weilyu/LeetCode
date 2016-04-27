ec14b5d1ab52ddd89421842712fa4c3e3f167bd5
weilyu <lvwei1001@live.com>
2016-04-27 18:30:09 +0800

    DeleteNodeinaLinkedList

---
+++ src/solutions/DeleteNodeinaLinkedList.java
@@ -0,0 +1,16 @@
+package solutions;
+
+/**
+ * Created by edwinlyu on 4/27/16.
+ * Total Accepted: 77843 Total Submissions: 178101 Difficulty: Easy
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ * <p>
+ * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should
+ * become 1 -> 2 -> 4 after calling your function.
+ */
+public class DeleteNodeinaLinkedList {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
