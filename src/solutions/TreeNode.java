2ed04f68a11a2f1634601173b6d8b4664941e64a
weilyu <lvwei1001@live.com>
2016-04-27 17:48:45 +0800

    Add TreeNode class

---
+++ src/solutions/TreeNode.java
@@ -0,0 +1,14 @@
+package solutions;
+
+/**
+ * Created by edwinlyu on 4/27/16.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
