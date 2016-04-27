b67954dab65b76ef04694a5776fb2408fc831f44
weilyu <lvwei1001@live.com>
2016-04-27 18:14:21 +0800

    Add ListNode class for future use

---
+++ src/solutions/ListNode.java
@@ -0,0 +1,16 @@
+package solutions;
+
+/**
+ * Created by edwinlyu on 4/27/16.
+ */
+
+//Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
