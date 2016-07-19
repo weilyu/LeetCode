ce0298eb04a1388f8a4a9861a669ba1e236528f8
wei <edwin.lyu@cn.ab-inbev.com>
2016-07-19 15:29:45 +0800

    ClimbingStairs

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Created by wei on 7/19/2016.
+ * https://leetcode.com/problems/climbing-stairs/
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ */
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        //TODO: Need optimization
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
