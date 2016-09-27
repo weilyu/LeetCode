8ddd3e2edb3ab5a5ff528b09139c33a601b8200b
weilyu <lvwei1001@live.com>
2016-09-27 20:56:40 +0800

    CountingBits

---
+++ src/solutions/CountingBits.java
@@ -0,0 +1,23 @@
+package solutions;
+
+/**
+ * Created by edwinlyu on 9/27/16.
+ */
+public class CountingBits {
+    public int[] countBits(int num) {
+        int[] output = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            output[i] = countOne(i);
+        }
+        return output;
+    }
+
+    private int countOne(int i) {
+        int numOne = 0;
+        String binary = Integer.toBinaryString(i);
+        for (char c : binary.toCharArray())
+            if (c == '1')
+                numOne++;
+        return numOne;
+    }
+}
